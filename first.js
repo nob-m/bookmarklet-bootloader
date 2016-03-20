acfb2e9b8d6b0c92678b68a5cb1f5f4511af9d6f
nob-m <m_nobuyuki@hotmail.com>
2016-03-21 03:22:54 +0900

    Update first.js

--- first.js
+++ first.js
@@ -1,32 +1,40 @@
 (function(){
 	var w = window;
-	if(typeof w.nob_m === 'undefined'){
-		w.nob_m = {};
+	if(typeof nob_m === 'undefined'){
+		nob_m = {};
 	}
-	if(typeof w.nob_m.common === 'undefined'){
-		w.nob_m.common = {};
-	}
-	w.nob_m.common.readExtJs = function(u, c, f){
-		var d = window.document;
-		try{
-			var u = u + '?r=' + Math.random();
-			var i = d.createElement('script');
-			i.src = u;
-			if(typeof c === 'string'){
-				i.charset = c;
+	nob_m.common = {
+		domain: 'https://nob-m.github.io/',
+		dir: 'boot_bookmarklet/',
+		class: {
+			prefix: 'nob_m_body_',
+			org: 'org',
+			add: 'add',
+			main: 'main',
+		},
+		readExtJs: function(u, c, f){
+			var d = w.document;
+			try{
+				u = u + '?r=' + Math.random();
+				var i = d.createElement('script');
+				i.src = u;
+				if(typeof c === 'string'){
+					i.charset = c;
+				}
+				if(typeof f === 'function'){
+					i.onload = f;
+				}
+				d.getElementsByTagName('head')[0].appendChild(i);
+			}catch(e){
+				alert(e);
 			}
-			if(typeof f === 'function'){
-				i.onload = function(){f();};
-			}
-			d.getElementsByTagName('head')[0].appendChild(i);
-		}catch(e){
-			alert(e);
 		}
 	};
-	w.nob_m.common.readExtJs('https://nob-m.github.io/js/jquery.min.js', 'utf-8', function(){
+	nob_m.common.readExtJs(nob_m.common.domain + '/js/jquery.min.js', 'utf-8', function(){
 		try{
+			$.support.cors = true;
 			$.ajax({
-				url: 'https://nob-m.github.io/bookmarklet/index.js',
+				url: nob_m.common.domain + nob_m.common.dir + 'index.js',
 				dataType: 'script',
 				scriptCharset: 'utf-8',
 				error: function(data){
@@ -35,6 +43,6 @@
 			});
 		}catch(e){
 			alert('データ読み込みに失敗しました。');
-		};
+		}
 	});
 })();
