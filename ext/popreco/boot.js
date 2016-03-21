234d9402c41eaf50ddac845823444266c47288a1
nob_m <m_nobuyuki@hotmail.com>
2016-03-22 04:50:19 +0900

    update ext/popreco

--- ext/popreco/boot.js
+++ ext/popreco/boot.js
@@ -8,5 +8,5 @@ $(function(){
 	$.noConflict(true);
 
 	//main
-	void((function(){var%20d=document;s=d.createElement('script');s.setAttribute('src','http://popreco.net/bookmarklet/popreco_get_data.js');d.body.appendChild(s);})());
+	void((function(){var d=document;s=d.createElement('script');s.setAttribute('src','http://popreco.net/bookmarklet/popreco_get_data.js');s.setAttribute('charset','utf-8');d.body.appendChild(s);})());
 });
